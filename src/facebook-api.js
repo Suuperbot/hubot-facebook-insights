2ec5b5875fac21d2353d2851dd268c645252883c
Adriano Godoy <godoy.ccp@gmail.com>
2016-04-26 09:46:20 -0300

    Adds fb node library. Create get fans count method

--- src/facebook-api.js
+++ src/facebook-api.js
@@ -2,23 +2,36 @@
 //   A facebook api script for hubot
 //
 // Configuration:
-//   LIST_OF_ENV_VARS_TO_SET
+//   FB_CLIENT_ID
+//   FB_CLIENT_SECRET
 //
 // Commands:
-//   hubot hello - <what the respond trigger does>
-//   orly - <what the hear trigger does>
-//
-// Notes:
-//   <optional notes required for the script>
+//   fb fans pageNameOrFBid - Get fans count from page/object with name 'pageNameOrFBid'
 //
 // Author:
 //   Adriano Godoy <godoy.ccp@gmail.com>
 
+var FB = require("fb");
+
 module.exports = function(robot) {
-  robot.respond(/hello/, function(res) {
-    return res.reply("hello!");
-  });
-  return robot.hear(/orly/, function() {
-    return res.send("yarly");
+  return robot.hear(/fb fans\s+(\w+)/i, function(res) {
+    var objectId = res.match[1];
+
+    FB.api("oauth/access_token", {
+        client_id: process.env.FB_CLIENT_ID,
+        client_secret: process.env.FB_CLIENT_SECRET,
+        grant_type: "client_credentials"
+      }, function (resFB) {
+        if(!resFB || resFB.error) {
+            return res.send(!resFB ? "error occurred" : resFB.error);
+        }
+
+        var accessToken = resFB.access_token;
+        var expires = resFB.expires ? resFB.expires : 0;
+
+        FB.api(objectId, { fields: ["fan_count"], access_token: accessToken }, function (res) {
+            return res.send(res["fan_count"]+" fans");
+        });
+    });
   });
 };
